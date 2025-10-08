2d2a183aca89020076e5a118f48e7fb5ae505d5e
shashank pandey <113227180+Shashank1115@users.noreply.github.com>
2025-10-08 21:29:25 +0530

    Time: 4 ms (24.65%), Space: 12.9 MB (14.89%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-    //  unordered_map<int,int> mp;
-    //  stack<int>st;
-    //  for(int x : nums2){
-    //     while(!st.empty() && x > st.top()){
-    //         mp[st.top()] = x;
-    //         st.pop();
-    //     }
-    //     st.push(x);
-    //  }
-    //  while(!st.empty()){
-    //     mp[st.top()] = -1;
-    //     st.pop();
-    //  }
-    //  vector<int> result;
-    //  for(int itr : nums1){
-    //     result.push_back(mp[itr]);
-    //  }
-    //  return result;
-    unordered_map<int,int>mp;
-    stack<int>st;
-    for(int it : nums2){
-        while(!st.empty() && it > st.top()){
-            mp[st.top()] = it;
-            st.pop();
-        }
-        st.push(it);
-    }
-    while(!st.empty()){
-        mp[st.top()] = -1;
+   unordered_map<int,int> mp;
+   stack<int> st;
+   for(int it : nums2 ){
+    while(!st.empty() && it > st.top()){
+        mp[st.top()] = it;
         st.pop();
     }
-    vector<int> ans;
-    for(int itr : nums1){
-        ans.push_back(mp[itr]);
-    }
-    return ans;
+    st.push(it);
+   }
+   while(!st.empty()){
+    mp[st.top()] = -1;
+    st.pop();
+   }
+   vector<int> ans;
+   for(int itr : nums1){
+     ans.push_back(mp[itr]);
+   }
+   return ans;
     }
 };
 
