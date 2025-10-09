abfae2614d6bd9d7f28b944408bac824b5f1731e
shashank pandey <113227180+Shashank1115@users.noreply.github.com>
2025-10-09 16:37:23 +0530

    Time: 15 ms (23.07%), Space: 40.9 MB (5.25%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -12,27 +12,25 @@
 class Solution {
 public:
     vector<vector<int>> result;
-    void find(TreeNode* root , int sum , vector<int> temp ,int& targetSum){
-        if(!root)
-            return;
-            sum += root -> val;
-            temp.push_back(root -> val);
-            if(root -> left == NULL && root -> right == NULL){
-                if(sum == targetSum){
-                    result.push_back(temp);
-                }
+    void find(TreeNode* root , int sum , vector<int> temp , int& targetSum){
+        if(!root) return;
+        sum += root -> val;
+        temp.push_back(root -> val);
+        if(root -> left == NULL && root -> right == NULL){
+            if(sum == targetSum){
+                result.push_back(temp);
             }
-            find(root -> left , sum , temp , targetSum);
-            find(root -> right , sum , temp , targetSum);
-        
+        }
+        find(root -> left ,sum , temp , targetSum);
+        find(root -> right , sum , temp , targetSum);
 
     }
-
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        int sum = 0;
+        int sum = 0 ;
         vector<int> temp;
-        
-        find(root , sum,temp,targetSum);
+        find(root,sum,temp,targetSum);
         return result;
+    
+        
     }
 };
