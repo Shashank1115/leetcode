ac0d1de803562b1c74d78ad4f85f6c9649507e0f
shashank pandey <113227180+Shashank1115@users.noreply.github.com>
2025-10-26 13:36:15 +0530

    Time: 0 ms (100%), Space: 24.6 MB (7.13%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -14,17 +14,31 @@ public:
 //     nums.push_back(0);
 //     }
 
-int j = 0 ;
-     for(int i  = 0 ; i < nums.size() ;i++){
-if(nums[i] != 0){
-    nums[j] = nums[i];
-    j++;
-}
-     }
-     while(j < nums.size()){
-        nums[j] = 0;
+// int j = 0 ;
+//      for(int i  = 0 ; i < nums.size() ;i++){
+// if(nums[i] != 0){
+//     nums[j] = nums[i];
+//     j++;
+// }
+//      }
+//      while(j < nums.size()){
+//         nums[j] = 0;
+//         j++;
+//      }   
+int n = nums.size();
+vector<int> arr(n,0);
+int i  = 0 , j = 0 ;
+while( i <  n){
+    if(nums[i] == 0){
+        i++;
+    }
+    else{
+        arr[j] = nums[i];
+        i++;
         j++;
-     }   
+    }
+} 
+nums = arr;
       } 
     
 };
