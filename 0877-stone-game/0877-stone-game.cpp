ed36e6495147aae5b177d6e82ab02ddbcf8ea81b
shashank pandey <113227180+Shashank1115@users.noreply.github.com>
2026-08-02 17:55:10 +0530

    Time: 18 ms (15.41%), Space: 19.6 MB (20.08%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-vector<vector<int>> dp;
-int helper(vector<int> &piles , int i , int j){
-    if( i == j){
-        return piles[i];
+    vector<vector<int>> dp;
+    int helper(vector<int>&piles , int i , int j){
+        if(i == j){
+            return piles[i];
+        }
+        if(dp[i][j]!= INT_MIN){
+            return dp[i][j];
+        }
+        int beginstone = piles[i] - helper(piles , i+1 , j);
+        int endstone = piles[j] - helper(piles , i , j-1);
+        return dp[i][j] = max(beginstone,endstone);
     }
-    if(dp[i][j] != INT_MIN){
-        return dp[i][j] ;
-    }
-   int beginstone = piles[i] - helper(piles,i+1,j);
-   int endstone = piles[j] - helper(piles , i ,j-1);
-    return dp[i][j] = max(beginstone,endstone);
-}
     bool stoneGame(vector<int>& piles) {
         dp.assign(piles.size(),vector<int>(piles.size(),INT_MIN));
-        return helper( piles , 0 , piles.size()-1) > 0 ? true : false;
+        return helper(piles , 0 , piles.size()-1) > 0 ? true : false;
+        
     }
 };
